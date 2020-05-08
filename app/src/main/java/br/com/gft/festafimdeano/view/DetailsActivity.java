63c198efaee9deee5ee0ef6fa674c5ca71a0a271
leandro-gft <lpsacchi@gmail.com>
2020-05-08 10:52:54 -0300

    Implementando confirmação de presença

--- app/src/main/java/br/com/gft/festafimdeano/view/DetailsActivity.java
+++ app/src/main/java/br/com/gft/festafimdeano/view/DetailsActivity.java
@@ -3,20 +3,41 @@ package br.com.gft.festafimdeano.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 
 import br.com.gft.festafimdeano.R;
+import br.com.gft.festafimdeano.constant.FimDeAnoConstants;
+import br.com.gft.festafimdeano.data.SecurityPreferences;
 
-public class DetailsActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSecurityPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
+        this.mSecurityPreferences = new SecurityPreferences(this);
+
         this.mViewHolder.checkParticipate = findViewById(R.id.check_participate);
+        this.mViewHolder.checkParticipate.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.check_participate){
+            if (this.mViewHolder.checkParticipate.isChecked()){
+                //SALVAR PRESENÇA
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_YES);
+            } else {
+                //SALVAR AUSENCIA
+                this.mSecurityPreferences.storeString(FimDeAnoConstants.PRESENCE_KEY, FimDeAnoConstants.CONFIRMATION_NO);
+
+            }
+        }
     }
 
     private static class ViewHolder{
